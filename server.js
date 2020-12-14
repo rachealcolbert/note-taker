9bc769b518240b9ac55697e8e28a5470e9b3725f
Racheal Colbert <rachealcolbert16@gmail.com>
2020-12-13 22:57:58 -0700

    app post

--- server.js
+++ server.js
@@ -2,14 +2,32 @@ const express = require('express');
 const {
     notes
 } = require('./data/notes.json');
+const fs = require('fs');
+const path = require('path');
 
 
 const PORT = process.env.PORT || 3001;
 const app = express();
+app.use(express.urlencoded({
+    extended: true
+}));
+app.use(express.json());
 
 function findById(id, notesArray) {
     const result = notesArray.filter(notes => notes.id === id)[0];
     return result;
+}
+
+function createNewNote(body, notesArray) {
+    const note = body;
+    notesArray.push(note);
+    fs.writeFileSync(
+        path.join(__dirname, './data/notes.json'),
+        JSON.stringify({
+            notes: notesArray
+        }, null, 2)
+    );
+    return note;
 }
 
 app.get('/', (req, res) => {
@@ -24,6 +42,12 @@ app.get('/api/notes', (req, res) => {
 app.get('/api/notes/:id', (req, res) => {
     const result = findById(req.params.id, notes);
     res.json(result);
+});
+
+app.post('/api/notes', (req, res) => {
+    req.body.id = notes.length.toString();
+    const notes = createNewNote(req.body, notes);
+    res.json(req.body);
 });
 
 app.listen(PORT, () => {
