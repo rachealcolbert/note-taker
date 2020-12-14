be92d941f5b7645b73034ec7a645dbe336d54f88
Racheal Colbert <rachealcolbert16@gmail.com>
2020-12-13 18:34:10 -0700

    connected data notes file

--- server.js
+++ server.js
@@ -3,3 +3,11 @@ const app = express();
 app.listen(3001, () => {
     console.log(`API server now on port 3001!`);
 });
+
+const {
+    notes
+} = require('./Data/notes.json');
+
+app.get('/api/notes', (req, res) => {
+    res.send('Hello!');
+});
